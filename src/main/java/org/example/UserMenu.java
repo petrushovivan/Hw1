a9b181cbff7c54f32717db0eaa0f50b32a0193e5
Petrushov Ivan <ewanpetrushov@yandex.ru>
2024-10-08 17:32:35 +0300

    1

--- src/main/java/org/example/UserMenu.java
+++ src/main/java/org/example/UserMenu.java
@@ -3,6 +3,7 @@ package org.example;
 import java.util.Scanner;
 
 public class UserMenu {
+    Scanner scanner = new Scanner(System.in);
     private User user;
     public UserMenu(User user) {
         this.user = user;
@@ -16,13 +17,33 @@ public class UserMenu {
                 " \n1. Change email\n2. Change password" +
                 "\n3. Change name\n4. Show habits" +
                 "\n5. Change habits\n6. Show my info");
-        Scanner scanner = new Scanner(System.in);
-        int choice = scanner.nextInt();
-        System.out.println(choice);
+        String choice = scanner.nextLine();
+        int number;
+        try {
+            number = Integer.parseInt(choice);
+        }
+        catch (NumberFormatException e) {
+            System.out.println("Please enter a number between 1 and 6");
+            showMenu();
+            return;
+        }
+        if(number == 1){
+            changeEmail();
+            showMenu();
+            return;
+        }
+        if(number == 6){
+            System.out.println(user.getName()+" "+user.getPassword()+" "+user.getEmail());
+            showMenu();
+            return;
+        }
     }
 
-    private void changeEmail(String email){
-        user.setEmail(email);
+    private void changeEmail(){
+        System.out.println("Please enter your new email address");
+        String newEmail = scanner.nextLine();
+        user.setEmail(newEmail);
+        System.out.println("Your new email address has been changed");
     }
 
     private void changePassword(String password){
