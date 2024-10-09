61817a0a9b6d769e0a988d1e43f9b894161d2b01
Petrushov Ivan <ewanpetrushov@yandex.ru>
2024-10-09 20:48:11 +0300

    Admin menu added

---
+++ src/main/java/org/example/Admin.java
@@ -0,0 +1,52 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Admin {
+
+    private Scanner scanner = new Scanner(System.in);
+
+    private ArrayList<User> users;
+
+    public Admin(ArrayList<User> users){
+        this.users = users;
+    }
+
+    public void showMenu(){
+        System.out.println("Welcome to the Admin Menu" +
+                "\n1. Watch users\n2. Remove user\n3. Exit");
+        String choice = scanner.nextLine();
+        int number;
+        try {
+            number = Integer.parseInt(choice);
+        }
+        catch (NumberFormatException e) {
+            System.out.println("Please enter a number between 1 and 3");
+            showMenu();
+            return;
+        }
+        if(number == 1){
+            for(User user : users){
+                System.out.println(user);
+            }
+            showMenu();
+            return;
+        }
+        else if(number == 2){
+            System.out.println("Enter the user email");
+            String email = scanner.nextLine();
+            for(User user : users){
+                if(user.getEmail().equals(email)){
+                    users.remove(user);
+                }
+            }
+            showMenu();
+            return;
+        }
+        else if(number == 3){
+            Main.main(null);
+            return;
+        }
+    }
+}
